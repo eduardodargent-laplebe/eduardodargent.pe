4379f9cf970821b723c00e5277cc090362c7aad2
Julio Garrido <juliogarridohidalgo@gmail.com>
2019-02-05 19:52:26 -0500

    gatsby config + componente cv + faltanombre.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,14 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+  
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: '9auv4erodoel'
+        accessToken: '4d3589cb4bf74cab9c8189a228a25964a609b855f5087a41e1a7a8c8f17bf9bf'
+      }
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
---
+++ src/components/Cv.js
@@ -0,0 +1,17 @@
+import React, { Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Cv extends Component{
+  render(){
+    return (
+    )
+  }
+}
+
+Cv.proptypes = {
+  data: PropTypes.object.isRequiered
+}
+
+export default CV 
+
+export const pageQuery = graphql 'query '
